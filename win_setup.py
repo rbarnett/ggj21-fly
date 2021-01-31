7ce5240dec46d80d249b16a80a6503d8af02a2c2
Rob Barnett <robertpbarnett@gmail.com>
2021-01-31 15:38:11 +0800

    Added windows distributable script

---
+++ win_setup.py
@@ -0,0 +1,10 @@
+import cx_Freeze
+# Change "App" to the name of your python script
+executables = [cx_Freeze.Executable("fly.py")]
+
+cx_Freeze.setup(
+    name="Fly",
+    version = "1",
+    options={"build_exe": {"packages":["pyglet", "cocos", "pygame"]}},
+    executables = executables
+    )
