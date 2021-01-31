d6ccc71eee3ad20c5e8d8af23d57b4e8d8e9ec45
Rob Barnett <robertpbarnett@gmail.com>
2021-01-31 15:47:43 +0800

    update Windows build script

--- win_setup.py
+++ win_setup.py
@@ -6,6 +6,6 @@ includefiles = ['fly.png', 'circle6.png']
 cx_Freeze.setup(
     name="Fly",
     version = "1",
-    options={"build_exe": {"packages":["pyglet", "cocos"]}},
+    options={"build_exe": {"packages":["pyglet", "cocos"], "include_files":includefiles}},
     executables = executables
     )
