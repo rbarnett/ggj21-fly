7c58c050eb77c3cf7c94456a0d8ec3c3835c0997
Rob Barnett <robertpbarnett@gmail.com>
2021-01-31 15:46:04 +0800

    update Windows build script

--- win_setup.py
+++ win_setup.py
@@ -1,6 +1,7 @@
 import cx_Freeze
 # Change "App" to the name of your python script
 executables = [cx_Freeze.Executable("fly.py")]
+includefiles = ['fly.png', 'circle6.png']
 
 cx_Freeze.setup(
     name="Fly",
